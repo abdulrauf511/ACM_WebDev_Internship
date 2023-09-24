7c3eda94711272d6f3f5ae7a6ea4fd3d2f4b29ec
abdulrauf511 <128225566+abdulrauf511@users.noreply.github.com>
2023-09-24 10:49:20 +0500

    Add files via upload

---
+++ Week _6 & Week_7/React Info Site/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import icon from "./images/react-icon-small.png";
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={icon} className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
